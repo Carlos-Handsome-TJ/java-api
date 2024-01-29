b265ec58a289f31db38d04274d4706e5ddbf270c
Jun <1356675512@qq.com>
2024-01-29 15:09:53 +0800

    build: 框架搭建

--- src/main/java/com/springboot/springbootquickstart/service/UserService.java
+++ src/main/java/com/springboot/springbootquickstart/service/UserService.java
@@ -1,10 +1,4 @@
 package com.springboot.springbootquickstart.service;
 
-import com.springboot.springbootquickstart.pojo.User;
-
-
 public interface UserService {
-  User findByUserName(String username);
-
-  void register(String username, String password);
 }
--- src/main/java/com/springboot/springbootquickstart/service/impl/UserServiceImpl.java
+++ src/main/java/com/springboot/springbootquickstart/service/impl/UserServiceImpl.java
@@ -1,25 +1,4 @@
 package com.springboot.springbootquickstart.service.impl;
 
-import com.springboot.springbootquickstart.mapper.UserMapper;
-import com.springboot.springbootquickstart.pojo.User;
-import com.springboot.springbootquickstart.service.UserService;
-import com.springboot.springbootquickstart.utils.Md5Util;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-  @Mapper
-  public UserMapper userMapper;
-  @Override
-  public User findByUserName(String username) {
-    User user = userMapper.findByUserName(username);
-    return user;
-  }
-
-  @Override
-  public void register(String username, String password) {
-    String psd = Md5Util.getMD5String(password);
-    userMapper.add(username, psd);
-  }
+public class UserServiceImpl {
 }
