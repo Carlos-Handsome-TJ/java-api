245a1c470bbbaa27316964d6201cc3584492b686
Jun <1356675512@qq.com>
2024-01-30 10:22:46 +0800

    feat: 新增登录接口

--- src/main/java/com/springboot/springbootquickstart/controller/UserController.java
+++ src/main/java/com/springboot/springbootquickstart/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springboot.springbootquickstart.controller;
 import com.springboot.springbootquickstart.pojo.Result;
 import com.springboot.springbootquickstart.pojo.User;
 import com.springboot.springbootquickstart.service.UserService;
+import com.springboot.springbootquickstart.utils.Md5Util;
 import jakarta.annotation.Resource;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.validation.annotation.Validated;
@@ -33,5 +34,18 @@ public class UserController {
       return Result.success();
     }
     return Result.error("用户名已被占用");
+  }
+  @PostMapping("/login")
+  public Result login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+    User user = userService.findByUserName(username);
+    if(user == null) {
+      return Result.error("用户不存在");
+    }
+    String userPsd = Md5Util.getMD5String(password);
+    String sqlPsd = user.getPassword();
+    if (userPsd.equals(sqlPsd)) {
+      return Result.success("jwt: token令牌，登陆成功");
+    }
+    return Result.error("密码错误");
   }
 }
