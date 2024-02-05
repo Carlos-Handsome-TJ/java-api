1e798fec620926350b7ad7277dae2ca7b5c3a522
Jun <1356675512@qq.com>
2024-02-05 09:07:39 +0800

    fix: 修复bean未注册问题

--- src/main/java/com/springboot/springbootquickstart/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/springboot/springbootquickstart/service/impl/ArticleServiceImpl.java
@@ -4,7 +4,9 @@ import com.springboot.springbootquickstart.mapper.ArticleMapper;
 import com.springboot.springbootquickstart.pojo.Article;
 import com.springboot.springbootquickstart.service.ArticleService;
 import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ArticleServiceImpl implements ArticleService {
   @Resource
   private ArticleMapper articleMapper;
