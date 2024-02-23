eb54a6f70a2adb4bb553c4b6dadfb4501c30d032
Jun <1356675512@qq.com>
2024-02-23 10:22:27 +0800

    fix: 增加自定义参数校验

--- src/main/java/com/springboot/springbootquickstart/pojo/Article.java
+++ src/main/java/com/springboot/springbootquickstart/pojo/Article.java
@@ -2,21 +2,26 @@ package com.springboot.springbootquickstart.pojo;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.groups.Default;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDateTime;
 
 // Article实体类
 @Data
 public class Article {
+  // 自定义参数校验
   @NotNull(message = "文章id不能为空", groups = {Update.class})
   private Integer id;
-  @NotNull(message = "文章标题不能为空")
+  @NotEmpty(message = "文章标题不能为空")
+  @Pattern(regexp = "^\\S(1,10)$", message = "标题长度为1-10个字符长度")
   private String title;
-  @NotNull(message = "文章内容不能为空")
+  @NotEmpty(message = "文章内容不能为空")
   private String content;
-  @NotNull(message = "文章封面不能为空")
+  @NotEmpty(message = "文章封面不能为空")
+  @URL
   private String coverImg;
   @NotNull(message = "发布状态不能为空")
   private String state;
