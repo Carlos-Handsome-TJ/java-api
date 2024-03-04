237fa46f28e59b8087e0ccabbc3c5481ebd2fb9d
jun <1356675512@qq.com>
2024-03-04 22:39:13 +0800

    feat: 新增上传文件接口

--- src/main/java/com/springboot/blog/controller/fileUpLoadController.java
+++ src/main/java/com/springboot/blog/controller/fileUpLoadController.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.pojo.Result;
+import com.springboot.blog.utils.AliOssUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,11 +13,11 @@ import java.util.UUID;
 @RestController
 public class fileUpLoadController {
   @PostMapping("/upload")
-  public Result<String> upload(MultipartFile file) throws IOException {
+  public Result<String> upload(MultipartFile file) throws Exception {
     // 获取文件内容的输入流，写入到本地磁盘中
     String originFilename = file.getOriginalFilename();
     String filename = UUID.randomUUID() + originFilename.substring(originFilename.lastIndexOf("."));
-    file.transferTo(new File("C:\\Users\\Administrator\\Desktop\\file\\" + filename));
-    return Result.success("访问url地址....");
+    String picUrl = AliOssUtil.uploadFile(filename, file.getInputStream());
+    return Result.success(picUrl);
   }
 }
